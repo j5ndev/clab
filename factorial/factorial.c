e9f49426734397a21d29d5758ed2f6c840f932ab
Michael Allan Jackson <m@j5n.dev>
2021-01-04 20:59:45 -0700

    Move exit out of validation function into the main function

--- factorial/factorial.c
+++ factorial/factorial.c
@@ -1,17 +1,34 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // Signatures of functions defined later
-int validateInput(int, char **);
 uintmax_t factorial(uintmax_t);
+struct Validation {
+   bool isValid;
+   char  message[50];
+};
+struct Validation validateInput(int, char **);
 
 // Entrypoint
 int main( int argc, char *argv[] )  {
-    uintmax_t value = validateInput(argc, argv);
-    printf("Input value: %lu\n", value);
+    // Validate input and exit if invalid
+    // TODO: additioanl validation checks, for example integer size
+    struct Validation check = validateInput(argc, argv);
+    if (check.isValid == false) {
+        puts(check.message);
+        exit(1);
+    }
+
+    // Convert input to integer
+    int value = atoi(argv[1]);
+
+    // Report input and calculated result
+    printf("Input value: %i\n", value);
     uintmax_t result = factorial(value);
-    printf("     Result: %lu\n", result);
+    printf("     Result: %li\n", result);
+
     return 0;
 }
 
@@ -30,14 +47,22 @@ uintmax_t factorial(uintmax_t value) {
 }
 
 // Validate input parameters
-int validateInput(int argc, char *argv[]) {
+struct Validation validateInput(int argc, char *argv[]) {
     if (argc != 2) {
-        puts("One argument expected.");
-        exit(1);
+        return (struct Validation) {
+            .message="One argument expected.",
+            .isValid=false,
+        };
     }
     if (argv[1][0] == '-') {
-        puts("Negative integers are not allowed.");
-        exit(1);
+        return (struct Validation){
+            .message="Negative integers are not allowed.",
+            .isValid=false,
+        };
     }
-    return atoi(argv[1]);
+    return (struct Validation) {
+        .message="All systems operational",
+        .isValid=true,
+    };
 }
+
